5516a9f9b7dc7618f38093c260f3748a9c77f479
Abdalrhman <lorgroup2002@gmail.com>
2022-07-17 13:54:47 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        //backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text("Welcome"),
+          backgroundColor: Colors.blue,
+          elevation: 3,
+          //centerTitle: true,
+          titleSpacing: 10,
+          //   leading: GestureDetector(
+          //     onTap: (){
+          //       print("BACK BUTTON PRESED");
+          //     },
+          //       child: const Icon(
+          //           Icons.arrow_back)),
+          leading: IconButton(
+            onPressed: () {
+              print("BACK BUTTON PRESED");
+            },
+            //iconSize: 30,
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 30,
+            ),
+            //color: Colors.black,
+          ),
+        ),
+        body: const SafeArea(
+          child: Center(
+            child: Text(
+              "Welcome in Flutter Course",
+              style: TextStyle(fontSize: 25, color: Colors.blue),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
